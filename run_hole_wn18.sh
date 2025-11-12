1054a28df89debb09e52603c6a2e278d75b7ab9a
Rodrigo Paganini <r.paganinipe@gmail.com>
2025-11-12 17:22:50 +0100

    add fout path to save trainings

--- run_hole_wn18.sh
+++ run_hole_wn18.sh
@@ -2,4 +2,5 @@
 
 python kg/run_hole.py --fin data/wn18.bin \
        --test-all 50 --nb 100 --me 500 \
-       --margin 0.2 --lr 0.1 --ncomp 150
+       --margin 0.2 --lr 0.1 --ncomp 150 \
+       --fout results/hole_wn18.pkl
