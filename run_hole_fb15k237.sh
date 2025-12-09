ca7a8aba3e02689d31c265bb7355dc971111f914
Rodrigo Paganini <r.paganinipe@gmail.com>
2025-12-09 23:09:44 +0100

    added training bash scripts

---
+++ run_hole_fb15k237.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+python kg/run_hole.py --fin data/fb15k-237.pkl \
+       --test-all 50 --nb 100 --me 500 \
+       --margin 0.2 --lr 0.1 --ncomp 150 \
+       --fout results/hole_fb15k237
